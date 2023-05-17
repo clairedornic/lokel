394f4b7e2cdd3a3b5f8cd689ed81a6413d915909
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-05-17 12:24:31 +0200

    feat: add custom fonts

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,11 +8,29 @@ import ProfilStack from './app/stacks/ProfilStack';
 import TranslateStack from './app/stacks/TranslateStack';
 import NavIcons from './app/components/NavIcons/NavIcons';
 import theme from './theme-design';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+	'Poppins-Light': require('./app/assets/fonts/Poppins-Light.ttf'),
+	'Poppins': require('./app/assets/fonts/Poppins-Regular.ttf'),
+	'Poppins-Medium': require('./app/assets/fonts/Poppins-Medium.ttf'),
+	'Poppins-SemiBold': require('./app/assets/fonts/Poppins-SemiBold.ttf'),
+	'Poppins-Bold': require('./app/assets/fonts/Poppins-Bold.ttf'),
+};
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+
+	const [isLoaded] = useFonts(customFonts);
+	if (!isLoaded) {
+		return null;
+	}
+	SplashScreen.hideAsync();
+
 	return (
 		<>
 			<NavigationContainer style={styles.nav}>
@@ -23,7 +42,7 @@ const App = () => {
 							return <NavIcons color={theme.colors.violet} iconName={route.name} focused={focused}></NavIcons>;
 						},
 						tabBarLabel: ({ tintColor, focused }) =>
-						focused ? (<Text style={{ fontWeight: 'bold', color: theme.colors.violet, fontSize: 16, borderBottomWidth: 3, borderBottomColor: theme.colors.violet, paddingBottom: 7 }} >{route.name}</Text>) : (<Text style={{ fontWeight: 'normal', color: theme.colors.violet, fontSize: 16, borderBottomWidth: 3, borderBottomColor: theme.colors.transparent, paddingBottom: 7 }} >{route.name}</Text>),
+						focused ? (<Text style={{ color: theme.colors.violet, fontSize: 16, borderBottomWidth: 3, borderBottomColor: theme.colors.violet, paddingBottom: 0, paddingTop: 2, fontFamily: 'Poppins-Bold' }} >{route.name}</Text>) : (<Text style={{ color: theme.colors.violet, fontSize: 16, borderBottomWidth: 3, borderBottomColor: theme.colors.transparent, paddingBottom: 0, paddingTop: 2, fontFamily: 'Poppins' }} >{route.name}</Text>),
 					})}
 				>
 					<Tab.Screen name="Apprendre" component={LearnStack} options={{ headerShown: false }} />
