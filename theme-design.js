cdef530155e283eb156655fe82cb0552b23113c7
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-05-17 10:49:13 +0200

    feat: add a theme for the app design

---
+++ theme-design.js
@@ -0,0 +1,46 @@
+const theme = {
+  colors: {
+    transparent: '#FFFFFF00',
+    white: '#fff',
+    black: '#131313',
+    gray: '#525252',
+    midGray: '#767676',
+    lightGray: '#DBDBDB',
+    blue: '#005FCE',
+    violet: '#7149DD',
+    midViolet: '#CEBBFF',
+    lila: '#EAE2FF',
+    orange: '#F0662B',
+    midOrange: '#FFA47D',
+    lightOrange: '#FFE3D8',
+    green: '#00890E',
+    midGreen: '#79D282',
+    lightGreen: '#C9F1CD',
+    yellow: '#FFD600',
+    midYellow: '#FFDC62',
+    lightYellow: '#FFF6C8'
+  },
+  spacing: {
+    extralarge: 35,
+    large: 25,
+    medium: 20,
+    small: 15,
+    extrasmall: 10,
+    thin: 8,
+    none: 0,
+  },
+  fontsize: {
+    large: 24,
+    medium: 20,
+    small: 16,
+  },
+  size: {
+    full: '100%',
+  },
+  radius: {
+    medium: 20,
+    small: 15,
+  },
+};
+
+export default theme;
