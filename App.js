566551396c31f6f3525abc08643863b149a27f11
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-05-17 10:49:54 +0200

    feat: add style for navigation

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
+import { StyleSheet, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LearnStack from './app/stacks/Learn.Stack';
 import ProfilStack from './app/stacks/ProfilStack';
 import TranslateStack from './app/stacks/TranslateStack';
-import  NavIcons from './app/components/NavIcons'
+import NavIcons from './app/components/NavIcons/NavIcons';
+import theme from './theme-design';
+
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
 	return (
 		<>
-			<NavigationContainer>
+			<NavigationContainer style={styles.nav}>
 				<Tab.Navigator
 					screenOptions={({ route }) => ({
-						tabBarIcon: ({ focused, color, size }) => {
-							return <NavIcons color={color} iconName={route.name}></NavIcons>;
+						tabBarStyle: { height: 70, borderTopColor: '#DEDEDE', borderTopWidth: 1, elevation: 0 },
+						tabBarItemStyle: { paddingTop: 4 },
+						tabBarIcon: ({ focused }) => {
+							return <NavIcons color={theme.colors.violet} iconName={route.name} focused={focused}></NavIcons>;
 						},
-						tabBarActiveTintColor: 'black',
-						tabBarInactiveTintColor: 'black',
+						tabBarLabel: ({ tintColor, focused }) =>
+						focused ? (<Text style={{ fontWeight: 'bold', color: theme.colors.violet, fontSize: 16, borderBottomWidth: 3, borderBottomColor: theme.colors.violet, paddingBottom: 7 }} >{route.name}</Text>) : (<Text style={{ fontWeight: 'normal', color: theme.colors.violet, fontSize: 16, borderBottomWidth: 3, borderBottomColor: theme.colors.transparent, paddingBottom: 7 }} >{route.name}</Text>),
 					})}
 				>
 					<Tab.Screen name="Apprendre" component={LearnStack} options={{ headerShown: false }} />
@@ -30,5 +35,14 @@ const App = () => {
 		</>
 	);
 }
+
+const styles = StyleSheet.create({
+	tabBarLabel: {
+	  fontWeight: 'bold',
+	},
+	nav: {
+		paddingVertical: theme.spacing.small,
+	}
+});
 
 export default App;
