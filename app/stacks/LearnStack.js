abcf2ae1ddae253e7f4aca0eb7f3ba7b3aaa78f7
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-05-29 00:50:47 +0200

    feat: add lesson screen to learn stack

--- app/stacks/LearnStack.js
+++ app/stacks/LearnStack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LearnScreen from '../screens/LearnScreen';
+import LessonScreen from '../screens/LessonScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,11 @@ const LearnStack = () => {
 			<Stack.Screen 
 				name="LearnScreen" 
 				component={LearnScreen} 
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen 
+				name="LessonScreen" 
+				component={LessonScreen} 
 				options={{ headerShown: false }}
 			/>
 		</Stack.Navigator>
