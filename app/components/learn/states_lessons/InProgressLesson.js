5c4ce2aa90fc40227395d834d8462a7758d9cb6a
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-06-07 21:13:12 +0200

    fix: imrpove style for InProgressLesson

--- app/components/learn/states_lessons/InProgressLesson.js
+++ app/components/learn/states_lessons/InProgressLesson.js
@@ -37,7 +37,7 @@ const InProgressLesson = ({lesson, navigation}) => {
 const styles = StyleSheet.create({
     cardContainer: {
         minWidth: 258,
-        paddingTop: 45,
+        paddingTop: 15,
     },
     illu: {
         display: 'flex',
